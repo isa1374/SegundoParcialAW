c2efb319d2544a35d0a0b1c5c8c4a386405431f4
loresfca <loresfca.flores1@gmail.cpm>
2016-10-29 15:06:32 -0500

    Ya saca el token asi bien moxxo omg

--- segundo-parcial/grails-app/controllers/segundo/parcial/UrlMappings.groovy
+++ segundo-parcial/grails-app/controllers/segundo/parcial/UrlMappings.groovy
@@ -3,15 +3,14 @@ package segundo.parcial
 class UrlMappings {
 
     static mappings = {
+        "/api/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
         "/divisasActivas"(controller:"activas", action:"consultar")
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
-
-        "/api/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
     }
 }
--- segundo-parcial/grails-app/init/BootStrap.groovy
+++ segundo-parcial/grails-app/init/BootStrap.groovy
@@ -6,12 +6,12 @@ import segundo.parcial.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-        User user = new User ("user","pass").save()
-        Role admin = new Role("ROLE_ADMIN").save()
-        UserRole.create(user,admin,true)
         new Divisa(imagen:'./images/us.png',nombre:'USD', valor:'1',convert:'false',ver:'2',disponible:'true').save()
         new Divisa(imagen:'./images/us.png',nombre:'USD', valor:'1',convert:'false',ver:'2',disponible:'true').save()
         new Divisa(imagen:'./images/us.png',nombre:'MXN', valor:'1',convert:'false',ver:'2',disponible:'true').save()
+        User user = new User ("user","pass").save()
+        Role admin = new Role("ROLE_ADMIN").save()
+        UserRole.create(user,admin,true)
     }
     def destroy = {
     }
